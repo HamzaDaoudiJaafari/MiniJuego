103c1bf2c502829e35e187199d76701f436a2770
Hamza Daoudi Jaafari <hamza.daoudi.jaafari@ces-vegamedia.es>
2024-02-21 09:53:20 +0100

    Agregar archivos de proyecto.

---
+++ VegaJuego/Level1.cs
@@ -0,0 +1,23 @@
+namespace VegaJuego
+{
+    public partial class Level1 : Form
+    {
+        Link Player;
+        public Level1()
+        {
+            InitializeComponent();
+            IniciarPersonajes();
+        }
+
+        private void IniciarPersonajes()
+        {
+            // Lienzo, coordenandaX, coordenadaY, salud, escudo, arma
+            Player = new Link(this, 0, 0, 30, 0, 0);
+        }
+          
+        private void Level1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Player.mover(e);
+        }
+    }
+}
